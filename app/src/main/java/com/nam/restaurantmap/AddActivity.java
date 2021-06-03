5fc3f58024920afc36c4c70539367ee5f2c41e09
PhuongNam <phiphuongnam1998@gmail.com>
2021-06-03 20:19:43 +0700

    1

--- app/src/main/java/com/nam/restaurantmap/AddActivity.java
+++ app/src/main/java/com/nam/restaurantmap/AddActivity.java
@@ -6,14 +6,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
+import android.location.Criteria;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -51,9 +56,9 @@ public class AddActivity extends AppCompatActivity implements LocationListener{
     EditText e1,e2;
     TextView t1,t2,t3;
     LocationDbHelper dbHelper;
-
+ 
     FusedLocationProviderClient client;
-    LocationManager locationManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,7 +68,6 @@ public class AddActivity extends AppCompatActivity implements LocationListener{
         t1=findViewById(R.id.tv1);
         t2=findViewById(R.id.tv2);
         t3=findViewById(R.id.tv3);
-
         client = LocationServices.getFusedLocationProviderClient(getApplicationContext());
 
         Places.initialize(getApplicationContext(),"AIzaSyDhNQLy-ez2DA7t3hNDmrlnQPh1PtuOPTM");
@@ -79,8 +83,7 @@ public class AddActivity extends AppCompatActivity implements LocationListener{
         t1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                getLocation();
-                //getCurentLocation();
+                getCurentLocation();
             }
         });
         t2.setOnClickListener(new View.OnClickListener() {
@@ -156,36 +159,13 @@ public class AddActivity extends AppCompatActivity implements LocationListener{
             }
         });
     }
-    private void getLocation()
+    public static boolean isLocationEnabled(Context context)
     {
-        FusedLocationProviderClient fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }else {
-            fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
-                @Override
-                public void onComplete(@NonNull Task<Location> task) {
-                    Location loc = task.getResult();
-                    if(loc!=null){
-                        try {
-                            Geocoder geocoder=new Geocoder(getApplicationContext(), Locale.getDefault());
-                            List<Address> addresses=geocoder.getFromLocation(loc.getLatitude(),loc.getLongitude(),1);
-                            e2.setText(addresses.get(0).getLatitude()+","+addresses.get(0).getLongitude());
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            });
-        }
+        //...............
+        return true;
     }
+
+
     private void getCurentLocation()
     {
         if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ) {
@@ -238,16 +218,9 @@ public class AddActivity extends AppCompatActivity implements LocationListener{
         }
     }
 
+
     @Override
     public void onLocationChanged(@NonNull Location location) {
-        try {
-            Geocoder  geocoder=new Geocoder(AddActivity.this,Locale.getDefault());
-        //    List<Address> addresses=geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
-         //   String ad=addresses.get(0).getAddressLine(0);
-          //  e2.setText(location.getLatitude()+","+location.getLongitude());
-        }catch (Exception e)
-        {
-            e.printStackTrace();
-        }
+
     }
 }
